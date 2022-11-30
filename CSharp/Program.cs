a9f849ede002b3d48fe919b4a20227a964731420
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-11-30 12:20:39 -0800

    Abstract Classes

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CSharp
 {
-    
-    class Program
+    /*Abstract Classes
+    Cannot be Sealed
+    */
+    public abstract /*sealed*/ class Customer
+    { 
+        /* May Contain Abstract members
+         Abstract member cannot have implementation 
+        
+         public abstract void printTest() { }
+    */    
+    public abstract void print();
+
+    }
+    //  If class does not wish to provide Implementation for all the abstract members inherited
+    //  Then the class has to be marked as abstract
+    public class /*Abstract*/ Program : Customer
     {
-        static void Main()
+        /*
+         An Non Abstract Class Derived from Abstract class Must provide Implementation for all abstract members .
+         Using override KEYWORD
+         */
+
+        public override void print()
+        {
+            Console.WriteLine("Hello From Print Abstract Method");
+        }
+
+        public static void Main()
         {
 
         }
