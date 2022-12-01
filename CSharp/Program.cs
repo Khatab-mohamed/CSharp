3a899c83e6dfc833adb65123d8d712aea60781fc
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-11-30 18:05:17 -0800

    Delegate Usage

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,27 +1,42 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CSharp
 {
-    /// <summary>
-    ///     Delegate is type safe function pointer
-    ///     Reference Type;
-    ///  
-    /// </summary>
-    public delegate void HelloFunctionDelegate(string message);
-
-
     public class Program
     {
-        
+
         public static void Main()
         {
-            var helloFunctionDelegate = new HelloFunctionDelegate(HelloMessage);
-            // we passing in the Name of the Function which we want the Delegate point To.
-            helloFunctionDelegate("Hi From Delegate"); 
+            IList<Employee> employees = new List<Employee>()
+            {
+                new Employee {Name = "omar", Id = 101, Salary = 5000, Experience = 5},
+                new Employee {Name = "Khalid", Id = 102, Salary = 4000, Experience = 4},
+                new Employee {Name = "Ali", Id = 103, Salary = 7000, Experience = 6},
+                new Employee {Name = "Han", Id = 104, Salary = 5000, Experience = 2}
+            };
+
+            Employee.PromoteEmployee(employees);
+
         }
-        public static void HelloMessage(string stringMessage)
+
+        public class Employee
         {
-            Console.WriteLine(stringMessage);
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public int Experience { get; set; }
+
+            public static void PromoteEmployee(IList<Employee> employees)
+            {
+                foreach (var employee in employees)
+                {
+                    if (employee.Experience >= 5)
+                        Console.WriteLine(employee.Name + " Promoted");
+                }
+
+            }
         }
     }
 }
