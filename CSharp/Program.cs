29991dc95bbd8a30e9499bc48223c469e26fb77f
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-12-02 04:35:19 -0800

    Multi Casting Delegates Usage

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,55 +4,43 @@ using System.Collections.Generic;
 
 namespace CSharp
 {
-    // Delegate Declaration 
-    public delegate bool IsPromoteEmployee(Employee employee);
+    public  delegate void SampleDelegate();
     public class Program
     {
-        /// <summary>
-        /// This is function which the Delegate can point To
-        /// </summary>
-        /// <param name="employee"></param>
-        /// <returns></returns>
-        public static bool PromotableCheck(Employee employee)
-        {
-            return employee.Experience >= 5;
-        }
         public static void Main()
         {
-            IList<Employee> employees = new List<Employee>()
-            {
-                new Employee {Name = "omar", Id = 101, Salary = 5000, Experience = 5},
-                new Employee {Name = "Khalid", Id = 102, Salary = 4000, Experience = 4},
-                new Employee {Name = "Ali", Id = 103, Salary = 7000, Experience = 6},
-                new Employee {Name = "Han", Id = 104, Salary = 5000, Experience = 2}
-            };
-            
-            //Delegate Initialization 
-            var isPromoteEmployee = new IsPromoteEmployee(PromotableCheck);
+            SampleDelegate del1, del2, del3,del4;
+            del1= new SampleDelegate(SampleMethodOne);
+            del2= new SampleDelegate(SampleMethodTow);
+            del3= new SampleDelegate(SampleMethodThree);
+            // Multi Cast Delegate:
+            // is Delegate which is pointing to More than One Function
+            //                          UnRegister Function using -=
+            del4 = del1 + del2 + del3 - del2;
+            del4();
+            //Using += to Register this functions To Delegate
+            SampleDelegate del5= new SampleDelegate(SampleMethodOne);
+            del5 += SampleMethodTow;
+            del5 += SampleMethodThree;
+            del5 -= SampleMethodOne;
+            Console.WriteLine("---------------------");
 
-            //Using the delegate as a parameter
-            Employee.PromoteEmployee(employees,isPromoteEmployee);
+            del5();
         }
 
-      
-
-    }
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Salary { get; set; }
-        public int Experience { get; set; }
-
-        public static void PromoteEmployee(IList<Employee> employees, IsPromoteEmployee isEligiblePromoteEmployee)
+        public static void SampleMethodOne()
         {
-            foreach (var employee in employees)
-            {
-                if (isEligiblePromoteEmployee(employee))
-                    Console.WriteLine(employee.Name + " Promoted");
-            }
-
+            Console.WriteLine("Sample Method One");
         }
+        public static void SampleMethodTow()
+        {
+            Console.WriteLine("Sample Method Two");
+        }
+        public static void SampleMethodThree()
+        {
+            Console.WriteLine("Sample Method Three");
+        }
+
     }
 
 }
