1123d8dec9d101377d28e9f44c23d81926ed37d3
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-12-04 04:47:41 -0800

    Dictionary

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,33 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharp
 {
+    /// <summary>
+    ///         Dictionary
+    /// 1- Dictionary is (key,value) Pairs
+    /// 2- It's present in System.Collections.Generic;
+    /// 
+
+    /// </summary>
     public class Program
     {
         public static void Main()
         {
-            
-            // 
-            AddNumbers(1,2,1,2,3,4,5);
-            //Second way to use optional parameters
-            AddNumbers(1,2,new object[] {1,2,3,4,5});
-        
-        }
-        //Optional Parameters using -->  params object[] 
-        public static void AddNumbers(int firstName, int secondNumber, params object[] restNumbers)
-        {
-            var result = firstName + secondNumber;
-            if (restNumbers!=null)
+            var customer1= new Customer(){Id = 110,Name = "Mark", Salary = 3000};
+            var customer2= new Customer(){Id = 115,Name = "Marry", Salary = 5000};
+            var customer3= new Customer(){Id = 119,Name = "John", Salary = 9000};
+
+            // 3- When Creating a dictionary, we need to specify the type of Key and Value.
+            // 4- Keys must be unique
+            var customersDictionary = new Dictionary<int, Customer>
             {
-                foreach (int number in restNumbers)
-                {
-                    result += number;
-                }
-                
-                Console.WriteLine(result);
+                {customer1.Id, customer1}, {customer2.Id, customer2}, {customer3.Id, customer3}
+            };
+
+            var customer119 = customersDictionary[119];
+            Console.WriteLine("\n ID :{0} \n Name : {1} \n Salary: {2} \n",customer119.Id,customer119.Name,customer119.Salary);
+            
+            // Using KeyValuePairs   
+            foreach (KeyValuePair<int,Customer> valuePairCustomer in customersDictionary)
+            {
+                Console.WriteLine("\n ID :{0}", valuePairCustomer.Key);
+
+                var customer = valuePairCustomer.Value;
+                Console.WriteLine("\n Name :{0}  Salary : {1} ", customer.Name,customer.Salary);
+                Console.WriteLine("------------------------------------------");
             }
         }
-        
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
     }
 
 }
