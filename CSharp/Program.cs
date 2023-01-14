8acafd9afee94e083226d675f674c58191c6b59b
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2023-01-14 12:00:32 -0800

    Hackerranck c# There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,58 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
 
-namespace CSharp
+class Result
 {
-    /// <summary>
-    ///     Dictionary
-    ///     1- Dictionary is (key,value) Pairs
-    ///     2- It's present in System.Collections.Generic;
-    /// </summary>
-    public class Program
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
     {
-        public static void Main()
+        var pairsList = new List<int>();
+        var pairsCount = 0;
+        ar.GroupBy(a=>a).ToList().ForEach(g => pairsList.Add(g.Count()));
+        foreach (var item in pairsList)
         {
-            var customer1 = new Customer {Id = 110, Name = "Mark", Salary = 3000};
-            var customer2 = new Customer {Id = 115, Name = "Marry", Salary = 5000};
-            var customer3 = new Customer {Id = 119, Name = "John", Salary = 9000};
-
-            // 3- When Creating a dictionary, we need to specify the type of Key and Value.
-            // 4- Keys must be unique
-            var customersDictionary = new Dictionary<int, Customer>
+            if (item > 0)
             {
-                {customer1.Id, customer1}, {customer2.Id, customer2}, {customer3.Id, customer3}
-            };
-            //  1-TryGetValue(key, out Value)
-            if (customersDictionary.TryGetValue(110, out var customerCasting))
-                Console.WriteLine("ID :{0}, Name:{1}, Salary:{2}", customerCasting.Id, customerCasting.Name,
-                    customerCasting.Salary);
-            else
-                Console.WriteLine("The Key is not found");
-            //  2- Count 
-            Console.WriteLine("Total Items = {0}", customersDictionary.Count);
-            //  3- Count()----> LINQ count Enumerable
-            Console.WriteLine("Salary More than 4000  = {0}", customersDictionary.Count(kvp =>kvp.Value.Salary > 4000));
-            //  4- Remove single value using the key
-            customersDictionary.Remove(key:110);
-            //  5- Clear --> Removing all items in the Dictionary
-            customersDictionary.Clear();
+                if (item % 2 == 0)
+                {
+                    pairsCount += item / 2;
+                }
+                else
+                {
+                    pairsCount+= (item-1) / 2;
+                    
+                }
+                }
 
-            // 6- convert an array into Dictionary
-
-            var customers = new Customer[3];
-            customers[0] = customer1;
-            customers[1] = customer2;
-            customers[2] = customer3;
-            var dictionary = customers.ToDictionary(c => c.Id, c => c);
+            }
+        return pairsCount;
         }
+      
     }
 
 
-    public class Customer
+class Solution
+{
+    public static void Main(string[] args)
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Salary { get; set; }
+
+        int n = 30;
+
+       var ar =  new List<int>() { 10,10,20,20,20, 30, 30, 30, 30, 30 };
+
+        int result = Result.sockMerchant(n, ar);
+
+        Console.WriteLine(result);
+
+       ;
     }
 }
